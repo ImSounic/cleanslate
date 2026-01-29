655db520ebeff5a4f08b9e27d287e789bd694371
Sounic Akkaraju <imsounic@Sounics-Mac-mini.local>
2026-01-30 00:03:10 +0100

    fix: update Android bundle ID to com.imsounic.cleanslate

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cleanslate"
+    namespace = "com.imsounic.cleanslate"
     compileSdk = 36  // Updated to 35 for notifications
     ndkVersion = "27.0.12077973"
     
@@ -23,7 +23,7 @@ android {
     
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cleanslate"
+        applicationId = "com.imsounic.cleanslate"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // Set to 21 for notifications
--- android/app/src/main/kotlin/com/imsounic/cleanslate/MainActivity.kt
+++ android/app/src/main/kotlin/com/imsounic/cleanslate/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cleanslate
+package com.imsounic.cleanslate
 
 import io.flutter.embedding.android.FlutterActivity
 
